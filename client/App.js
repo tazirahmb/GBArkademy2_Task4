fe7db7d65e0afbdba32099258f78f549ba1005d1
tazirahmb <tazirahmb@outlook.com>
2018-12-12 15:28:54 +0700

    connect front end to back end

--- client/App.js
+++ client/App.js
@@ -1,49 +1,189 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { 
+  Header,
+  ListItem,
+  Container,
+  Left,
+  Body,
+  Right,
+  Title,
+  Content,
+  Row,
+  Col
+ } from "native-base";
+import {
+  TextInput,
+  Button,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+import axios from 'axios';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  constructor() {
+    super(); 
+    this.state = {
+      text: '',
+      listan: [],
+      id: '',
+      submitButton: 'Submit',
+      refreshStatus: true
+    }
+  }
+
+  componentDidMount() {
+    this.loadToDoList();
+  }
+
+  loadToDoList() {
+    this.setState({refreshStatus: true})
+    console.log('cek todolist');
+    axios.get('http://192.168.0.19:3001/api/todo')
+    .then((res) => {
+      console.log('data get')
+      console.log(res)
+      this.setState({
+        listan: res.data.data,
+        refreshStatus: false
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  handleClick = () => {
+    if(this.state.submitButton === 'Submit') {
+      this.postUpdate();
+    } else if (this.state.submitButton === 'Update') {
+      this.editItem(this.state.id);
+    }
+  }
+
+  postUpdate = () => {
+    axios.post('http://192.168.0.19:3001/api/todo', {
+      text: this.state.text
+    })
+    .then((res) => {
+      console.log(res);
+      console.log(this.state.text + ' berhasil dimasukkan');
+      this.setState({
+        text: ''
+      });
+      this.loadToDoList();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  editItem = (item) => {
+    console.log(item);
+    axios.put('http://192.168.0.19:3001/api/todo/' + item, {
+      text: this.state.text
+    })
+    .then((res) => {
+      console.log(res); 
+      console.log('updated');
+      this.loadToDoList();
+      this.setState({
+        text: '',
+        id: '',
+        submitButton: 'Submit'
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    }) ;
+  }
+
+  onEditItemClicked = (item) => {
+    this.setState({
+      submitButton: 'Update',
+      text: item.text,
+      id: item._id
+    })
+    console.log('id : ' + item._id);
+  }
+
+  deleteItem = (item) => {
+    axios.delete('http://192.168.0.19:3001/api/todo/' + item._id)
+    .then((res) => {
+      console.log(res); 
+      console.log('todo deleted');
+      this.loadToDoList();
+    })
+    .catch((err) => {
+      console.log(err);
+    }) ;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Header style={{backgroundColor: 'black'}} androidStatusBarColor='#222' >
+          <Left/>
+          <Body>
+            <Title>TO-DO</Title>
+          </Body>
+          <Right/>
+        </Header>
+
+        <Content>
+          <Row  style={style.inputan}>
+            <Col size={3}>
+              <TextInput
+                value={this.state.text}
+                ref={input => {this.textInputan = input}}
+                style={style.textInput}
+                placeholder='type here...'
+                onChangeText={(text) => this.setState({text})}
+                ></TextInput>
+            </Col>
+            <Col size={1}>
+              <Button
+                  title={this.state.submitButton}
+                  color="black"
+                  onPress={this.handleClick}
+                  ></Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <FlatList
+              inverted
+              refreshing={this.state.refreshStatus}
+              data={this.state.listan}
+              keyExtractor={(item, index) => item._id}
+              renderItem={({item}) => 
+                <ListItem>
+                  <TouchableOpacity
+                    onPress={() => this.onEditItemClicked(item)}
+                    onLongPress={() => this.deleteItem(item)}
+                  >
+                    <Text>{item.text}</Text>
+                  </TouchableOpacity>
+                </ListItem>
+            }
+            />
+          </Row>
+        </Content>
+
+  </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const style = StyleSheet.create({
+  inputan: {
+    padding: 16
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  textInput: {
+    borderBottomColor: '#AAA',
+    borderBottomWidth: 0.5,
+    marginRight: 8
+  }
+})
